4aea83aad9963b897da6a6f0ca07398e0d353743
DimeDiose18 <dimediose09@gmail.com>
2023-08-28 16:19:05 -0500

    feat(api): :sparkles: Create paths for /pokemons.

--- api/src/controllers/demo.js
+++
@@ -1,6 +0,0 @@
-//const infoPokemons = await axios.get(url)
-//.then(data.results)
-
-const infoPokemons = async () => {
-  await axios.get(url).then((response) => console.log(response.data.results));
-};
--- api/src/routes/pokemonsRouter.js
+++ api/src/routes/pokemonsRouter.js
@@ -1,38 +1,110 @@
 const { Router } = require("express");
-const getApiInfo = require("../controllers/getPokemons");
+const { Pokemon, Type } = require("../db");
+const getAllPokemons = require("../controllers/getAllPokemons");
+const { default: axios } = require("axios");
+const filterInfo = require("../utils/filterInfo");
 
 const pokemonsRouter = Router();
 //GET /pokemons
 pokemonsRouter.get("/", async (req, res) => {
+  const { page } = req.body;
   try {
-    const allPokemons = await getApiInfo();
-    res.json(allPokemons);
+    const allPokemons = await getAllPokemons(page);
+    res.status(200).json(allPokemons);
   } catch (error) {
-    res.status(500).json({ "error": "No hay pokemons" });
+    res.status(500).json({ error: "No hay pokemons" });
   }
 });
-// GET /pokemons/id
-pokemonsRouter.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const ID = parseInt(id);
-  const allPokemonsById = await getApiInfo();
-    if(ID) {
-      const detailPokemon = allPokemonsById.find(pokemon => pokemon.id === ID);
-      res.json(detailPokemon).status(200, console.log("pokemon encontrado"))
-    } else{
-      res.status(500, console.log("pokemon no encontrado"));
+// GET /pokemons/name
+
+pokemonsRouter.get("/name", async (req, res) => {
+  const { name } = req.query;
+  const allPokemons = await axios
+    .get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`)
+    .then((response) => response.data)
+    .catch((error) => console.error(error));
+
+  const findPokemonDB = async () => {
+    const data = await Pokemon.findAll({
+      where: { name: name.toLowerCase() },
+    });
+    return data;
+  };
+
+  const data = allPokemons ? filterInfo(allPokemons) : await findPokemonDB();
+
+  try {
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.status(404).json({ message: "Not Found Pokemon" });
     }
-  } 
-);
-// GET pokemons/name?=
-pokemonsRouter.get("/", function (req, res) {
-  const {name} = req.body;
-  res.send(`Estoy en la ruta de ${name}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
 });
 
-pokemonsRouter.post("/", (req, res) => {
+// GET /pokemons/id
 
-  res.send("Estoy en la ruta de Post");
+pokemonsRouter.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const allPokemons = await axios
+    .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then((response) => response.data);
+  const data = filterInfo(allPokemons);
+  try {
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.status(404).json({ message: "Not Found Pokemon" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+//POST /pokemons
+
+pokemonsRouter.post("/", async (req, res) => {
+  const {
+    name,
+    img,
+    hp,
+    attack,
+    specialAttack,
+    defense,
+    specialDefense,
+    speed,
+    height,
+    weight,
+    types,
+  } = req.body;
+  try {
+    let newPokemon = await Pokemon.create({
+      name,
+      img,
+      hp,
+      attack,
+      specialAttack,
+      defense,
+      specialDefense,
+      speed,
+      height,
+      weight,
+    });
+
+    let typeDB = await Type.findAll({
+      where: { name: types },
+    });
+
+    newPokemon.addType(typeDB);
+
+    res.status(200).json({ message: "El pokemon ha sido creado con exito" });
+  } catch (error) {
+    res.status(500).json({ message: "Error de servidor", error });
+  }
 });
 
 module.exports = pokemonsRouter;
