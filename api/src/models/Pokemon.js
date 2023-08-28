81b678aefac908f98879a34399772a7993799125
DimeDiose18 <dimediose09@gmail.com>
2023-08-28 15:42:28 -0500

    feat(api): :sparkles: Add model for Pokemon.

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -6,68 +6,42 @@ module.exports = (sequelize) => {
   sequelize.define("pokemon", {
     id: {
       type: DataTypes.UUID,
-      primaryKey: true,
-      allowNull: false,
       defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
     },
     name: {
       type: DataTypes.STRING,
-      unique: true,
       allowNull: false,
     },
     image: {
       type: DataTypes.STRING,
       allowNull: true,
-      validate: { isUrl: true },
       defaultValue: "https://i.imgur.com/R1WxMTs.png",
     },
     hp: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      validate: {
-        min: 0,
-        max: 255,
-      }
     },
     attack: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      validate: {
-        min: 0,
-        max: 200,
-      }
     },
     specialAttack: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      validate: {
-        min: 0,
-        max: 200,
-      }
     },
     defense: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      validate: {
-        min: 0,
-        max: 250,
-      }
     },
     specialDefense: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      validate: {
-        min: 0,
-        max: 250,
-      }
     },
     speed: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      validate: {
-        min: 0,
-        max: 200,
-      }
     },
     height: {
       type: DataTypes.DECIMAL,
