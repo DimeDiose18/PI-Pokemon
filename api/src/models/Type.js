a699e6c879e746a468def6a563f478815bb249aa
DimeDiose18 <dimediose09@gmail.com>
2023-08-28 15:58:08 -0500

    feat(api): :sparkles: Add model for each type of pokemon.

--- api/src/models/Type.js
+++ api/src/models/Type.js
@@ -1,15 +1,28 @@
 const { DataTypes } = require("sequelize");
+
 module.exports = (sequelize) => {
-  sequelize.define("type", {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
+  sequelize.define(
+    "type",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+
+      name: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+
+      image: {
       type: DataTypes.STRING,
       allowNull: false,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
