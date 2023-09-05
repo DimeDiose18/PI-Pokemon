2debd1094783d908f64ebfdfce25847c3f4f9e9d
DimeDiose18 <dimediose09@gmail.com>
2023-09-04 20:57:16 -0500

    feat(client): :sparkles: Add and define routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-ReactDOM.render(
+import Home from "./routes/Home.jsx";
+import Landing from "./routes/Landing.jsx";
+import Profile from "./routes/Profile.jsx";
+import CreatePokemon from "./routes/CreatePokemon.jsx";
+import Detail from "./routes/Detail";
+import Error from "./routes/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+    children: [
+      {
+        path: "/home/pokemons/:pokemonsid",
+        element: <Detail />,
+      },
+    ],
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/createPokemon",
+    element: <CreatePokemon />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
