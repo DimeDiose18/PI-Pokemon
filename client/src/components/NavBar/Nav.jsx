7335650cede302c7d62aa6b94f0ae307d9dfb40d
DimeDiose18 <dimediose09@gmail.com>
2023-09-04 21:16:37 -0500

    feat(client): :sparkles: Define a nav bar

---
+++ client/src/components/NavBar/Nav.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import styles from "./Nav.module.css";
+import logoPokemon from "../../assets/images/LogoGrande.png";
+import SearchBar from "./SearchBar/SearchBar";
+
+const Nav = () => {
+    return (
+        <nav className={styles.navBar}>
+        <div className={styles.logoContainer}>
+            <img src={logoPokemon} alt="Logo Pokemon" className={styles.logo}/>
+        </div>
+        <div>
+            <SearchBar />
+        </div>
+            <ul>
+                <li>
+                    <Link to={'/home'}> Home </Link>
+                </li>
+                <li>
+                    <Link to={'/profile'}> Profile </Link>
+                </li>
+                <li>
+                    <Link to={'/createPokemon'}> Create Pokemon </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+};
+
+export default Nav;
