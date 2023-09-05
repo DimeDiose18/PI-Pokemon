abb6d2d424a83c2724d86074b894e65d798c2c9d
DimeDiose18 <dimediose09@gmail.com>
2023-09-04 21:07:06 -0500

    feat(client): :sparkles: Define path for Profile page

---
+++ client/src/routes/Profile.jsx
@@ -0,0 +1,16 @@
+import Footer from "../components/Footer/Footer";
+import Log from "../components/Login/Log";
+import Nav from "../components/NavBar/Nav";
+
+const Profile = () => {
+  return (
+    <div>
+      <Log />
+      <Nav />
+      <h1> Soy el Profile</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default Profile;
