8583c75d854471d7634a98582e191ae7ac3f69c2
DimeDiose18 <dimediose09@gmail.com>
2023-09-06 23:37:10 -0500

    feat(client): :sparkles: Settings pokemon detail component

--- client/src/routes/Detail.jsx
+++ client/src/routes/Detail.jsx
@@ -1,12 +1,47 @@
+import { useEffect, useState} from "react"
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
+//import { fetchPokemonDetails } from "../redux/actions";
+import imageLoading from "../assets/images/Poké_Ball_icon.svg.png";
+import { fetchPokemonDetails } from "../redux/actions";
+ 
 const Detail = () => {
-   const { pokemonsid } = useParams();
+   const {pokemonName} = useParams();
+   const [pokemonDetail, setPokemonDetail] = useState()
+   //const [pokemones, setPokemones]= useState();
+
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+    dispatch(fetchPokemonDetails(pokemonName))
+    .then((response)=>{
+        const data  = response.payload
+        setPokemonDetail(data)
+    })
+    .catch((error) => {
+        console.error("Error al obtener detalles del pokemon", error)
+    })
+   }, [dispatch, pokemonName]);
+
+if(!pokemonDetail) {
     return (
         <div>
-            <h3>Soy el detail {pokemonsid}</h3>
+            <img width="100" src={imageLoading} alt="loading..."/>
         </div>
-    )
+    );
+} else {
+ return (
+        <div>
+            <h3>Soy el detail de:{pokemonName}</h3>
+            <div>
+                <h2>{pokemonDetail.name}</h2>
+                <img src={pokemonDetail.img} alt="detail-pokemon"/>
+            </div>
+        </div>
+    )   
+}
+    
 };
 
 export default Detail;
