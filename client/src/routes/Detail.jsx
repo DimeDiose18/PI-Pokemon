1a98612510383504f42cca8c2234657e5a8e9f85
DimeDiose18 <dimediose09@gmail.com>
2023-09-15 15:08:18 -0500

    refactor(client): :recycle: Finished pokemon details

--- client/src/routes/Detail.jsx
+++ client/src/routes/Detail.jsx
@@ -1,47 +1,118 @@
-import { useEffect, useState} from "react"
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
-
-//import { fetchPokemonDetails } from "../redux/actions";
+import { useParams, Link } from "react-router-dom";
+import styles from "../components/Home/Detail.module.css";
 import imageLoading from "../assets/images/Poké_Ball_icon.svg.png";
 import { fetchPokemonDetails } from "../redux/actions";
- 
+
 const Detail = () => {
-   const {pokemonName} = useParams();
-   const [pokemonDetail, setPokemonDetail] = useState()
-   //const [pokemones, setPokemones]= useState();
-
-   const dispatch = useDispatch();
-
-   useEffect(() => {
+  const { pokemonName } = useParams();
+  const [pokemonDetail, setPokemonDetail] = useState();
+  const dispatch = useDispatch();
+  useEffect(() => {
     dispatch(fetchPokemonDetails(pokemonName))
-    .then((response)=>{
-        const data  = response.payload
-        setPokemonDetail(data)
-    })
-    .catch((error) => {
-        console.error("Error al obtener detalles del pokemon", error)
-    })
-   }, [dispatch, pokemonName]);
+      .then((response) => {
+        const data = response.payload;
+        setPokemonDetail(data);
+      })
+      .catch((error) => {
+        console.error("Error getting pokemon details", error);
+      });
+  }, [dispatch, pokemonName]);
 
-if(!pokemonDetail) {
+  const handleBackHome = () => {
+    setPokemonDetail();
+
+  };
+
+  if (!pokemonDetail) {
     return (
-        <div>
-            <img width="100" src={imageLoading} alt="loading..."/>
-        </div>
+      <div className={styles.containerLoading}>
+        <img
+          className={styles.imageLoading}
+          src={imageLoading}
+          alt="loading..."
+        />
+      </div>
     );
-} else {
- return (
-        <div>
-            <h3>Soy el detail de:{pokemonName}</h3>
-            <div>
-                <h2>{pokemonDetail.name}</h2>
-                <img src={pokemonDetail.img} alt="detail-pokemon"/>
-            </div>
+  } else {
+    return (
+      <div className={styles.sideBarRight}>
+        <Link onClick={handleBackHome} to={"/home"}>
+          <button className={styles.buttonClose}>X</button>
+        </Link>
+        <div className={styles.containerID}>
+          <h6>ID: {pokemonDetail.id}</h6>
         </div>
-    )   
-}
-    
+        <div className={styles.containerMain}>
+          <div className={styles.containerTypes}>
+            <h3 className={styles.titleType}>Types:</h3>
+            {pokemonDetail.types.map((e) => (
+              <div key={e.name} className={styles.typeDetail}>
+                <p className={styles.pTypeDetail}>{e.name}</p>
+                <img
+                  src={e.image}
+                  className={styles.imgTypeDetail}
+                  alt="pokemon types"
+                />
+              </div>
+            ))}
+          </div>
+          <div className={styles.pokemonName}>
+            <h1>{pokemonDetail.name}</h1>
+            <img
+              className={styles.pokemonDetailImg}
+              src={pokemonDetail.image ? pokemonDetail.image : pokemonDetail.altImage}
+              alt="detail-pokemon"
+            />
+          </div>
+          <div className={styles.containerSpecs}>
+            <h3 className={styles.specs}>Height:</h3>
+            <p>{pokemonDetail.height}</p>
+            <h3 className={styles.specs}>Weight:</h3>
+            <p>{pokemonDetail.weight}</p>
+          </div>
+        </div>
+
+        <div className={styles.stats}>
+          <div className={styles.statsName}>
+            <div className={styles.h2}>
+              <h2>Stats:</h2>
+            </div>
+          </div>
+
+          <div className={styles.statsContainer}>
+            <div className={styles.statsUnit}>
+              <p className={styles.pTittle}>HP:</p>
+              <p>{pokemonDetail.hp}</p>
+            </div>
+            <div className={styles.statsUnit}>
+              <p className={styles.pTittle}>Attack:</p>
+              <p>{pokemonDetail.attack}</p>
+            </div>
+            <div className={styles.statsUnit}>
+              <p className={styles.pTittle}>SpecialAttack:</p>
+              <p>{pokemonDetail.specialAttack}</p>
+            </div>
+          </div>
+          <div className={styles.statsContainer}>
+            <div className={styles.statsUnit}>
+              <p className={styles.pTittle}>Defense:</p>
+              <p>{pokemonDetail.defense}</p>
+            </div>
+            <div className={styles.statsUnit}>
+              <p className={styles.pTittle}>SpecialDefense:</p>
+              <p>{pokemonDetail.specialDefense}</p>
+            </div>
+            <div className={styles.statsUnit}>
+              <p className={styles.pTittle}>Speed:</p>
+              <p>{pokemonDetail.speed}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Detail;
