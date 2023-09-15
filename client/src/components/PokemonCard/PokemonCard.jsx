3ac4b43d3fa919798e227df9623dc7a081cfc5d0
DimeDiose18 <dimediose09@gmail.com>
2023-09-15 14:58:05 -0500

    refactor(client): :recycle: Adjust pokemon card

--- client/src/components/PokemonCard/PokemonCard.jsx
+++ client/src/components/PokemonCard/PokemonCard.jsx
@@ -2,18 +2,22 @@ import styles from "./PokemonCard.module.css";
 
 const PokemonCard = ({ pokemon }) => {
   return (
-    <div className={styles.cardContainer} key={pokemon.id}>
-      <img className={styles.pokemonImage} src={pokemon.img} alt="pokemon" />
+    <div className={styles.cardContainer} key={pokemon?.id}>
+      <img className={styles.pokemonImage} src={pokemon?.image ? pokemon.image : pokemon.altImage} alt="pokemon" />
 
       <div className={styles.mainContainer}>
         <div className={styles.nameContainer}>
-          <h1 className={styles.pokemonName}>{pokemon.name}</h1>
+          <h1 className={styles.pokemonName}>{pokemon?.name}</h1>
         </div>
         <div className={styles.typesContainer}>
-          {pokemon.types.map((e) => (
-            <div key={e.name} className={styles.type}>
-              <p className={styles.pType}>{e.name}</p>
-              <img src={e.img} className={styles.imgType} alt="pokemon types" />
+          {pokemon?.types.map((e) => (
+            <div key={e?.name} className={styles.type}>
+              <p className={styles.pType}>{e?.name}</p>
+              <img
+                src={e?.image}
+                className={styles.imgType}
+                alt="pokemon types"
+              />
             </div>
           ))}
         </div>
