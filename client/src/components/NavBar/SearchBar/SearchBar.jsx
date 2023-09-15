d2f77a2ca9acb9535f72836c1e75823e9d226e10
DimeDiose18 <dimediose09@gmail.com>
2023-09-15 15:01:02 -0500

    feat(client): :sparkles: SearchBar finish

--- client/src/components/NavBar/SearchBar/SearchBar.jsx
+++ client/src/components/NavBar/SearchBar/SearchBar.jsx
@@ -1,13 +1,25 @@
 import styles from "./SearchBar.module.css";
-import search from "../../../assets/images/lupa-morada.png";
+import search from "../../../assets/images/lupa-verde.png";
 
-const SearchBar = () => {
-    return (
-        <div className={styles.containerSearch}>
-        <input type="text" placeholder="Find a Pokemon"/>
-            <button className={styles.searchImg}><img src={search} alt="img search"/></button>
-        </div>
-    )
-}
+const SearchBar = ({setSearchTerm}) => {
+
+  const handleSearchChange = (e) => {
+    const searchTerm = e.target.value;
+    setSearchTerm(searchTerm)
+  };
+  return (
+    <div className={styles.containerSearch}>
+      <input
+        type="text"
+        placeholder="Catch a Pokemon!"
+        className={styles.searchBox}
+        onChange={handleSearchChange}
+      />
+      <button className={styles.searchImg}>
+        <img src={search} alt="img search" />
+      </button>
+    </div>
+  );
+};
 
 export default SearchBar;
