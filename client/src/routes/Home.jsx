4e020523cc2d05d2bce0b3a6109b211d9aee1b11
DimeDiose18 <dimediose09@gmail.com>
2023-09-15 15:10:29 -0500

    refactor(client): :recycle: The views from each side were adjusted

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,23 +1,34 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Footer/Footer.jsx";
 import Log from "../components/Login/Log.jsx";
 import Nav from "../components/NavBar/Nav.jsx";
 import styles from "../components/Home/Home.module.css";
 import SideBarLeft from "../components/Home/SideBarLeft.jsx";
+import imageLoading from "../assets/images/Poké_Ball_icon.svg.png";
+import { useState } from "react";
 
 const Home = () => {
+  const [searchTerm, setSearchTerm] = useState();
+  const location = useLocation();
+  const path = location.pathname;
   return (
     <div className={styles.mainContainer}>
       <Log />
-      <Nav />
+      <Nav setSearchTerm={setSearchTerm} />
       <div className={styles.viewContainer}>
         <div className={styles.sideBarLeft}>
-          <SideBarLeft />
+          <SideBarLeft searchTerm={searchTerm} />
         </div>
         <div className={styles.sideBarRight}>
-          <Link to={"/home"}>
-            <button>X</button>
-          </Link>
+          {path === "/home" && (
+            <div className={styles.containerLoading}>
+              <img
+                className={styles.imageLoading}
+                src={imageLoading}
+                alt="loading..."
+              />
+            </div>
+          )}
           <Outlet />
         </div>
       </div>
