81815c03b8837ce72804543b2265077ee35b2fef
DimeDiose18 <dimediose09@gmail.com>
2023-09-15 15:09:42 -0500

    feat(client): :sparkles: Finished the pokemon views and additions

--- client/src/components/Home/SideBarLeft.jsx
+++ client/src/components/Home/SideBarLeft.jsx
@@ -1,76 +1,126 @@
-//es el verde el del scrool
-//vista de cada card de pokemon
 import React, { useEffect, useState } from "react";
 import styles from "./SideBarLeft.module.css";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { fetchPokemons } from "../../redux/actions.js";
+import { fetchAllTypes, fetchPokemons } from "../../redux/actions.js";
 import PokemonCard from "../PokemonCard/PokemonCard";
 import Loader from "../../components/Home/Loader.jsx";
 import Pagination from "../Pagination/Pagination.jsx";
+import FiltersBar from "../Filters/FiltersBar";
 
-
-const SideBarLeft = () => {
+const SideBarLeft = ({ searchTerm }) => {
   const [pokemones, setPokemones] = useState();
   const [currentPage, setCurrentPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
-  const [searchPage, setSearchPage] = useState(currentPage);
+  const [totalPages, setTotalPages] = useState();
+  const [allTypes, setAllTypes] = useState();
+  const [selectedType, setSelectedType] = useState("All");
+  const [selectedOrder, setSelectedOrder] = useState({
+    order: "",
+    typeOrder: "",
+  });
 
   const dispatch = useDispatch();
 
-  useEffect(()=> {
-    setIsLoading(true)
-    if(isLoading){
-    dispatch(fetchPokemons(searchPage))
-    .then((response) => {
-      const data = response.payload;
-      setPokemones(data)
-    }).then(()=>{
-      setIsLoading(false)
-    }).catch((error) => {
-      console.error("Error al obtener los pokemons", error)
-    })
-  }
-  }, [dispatch, searchPage, isLoading]);
+  useEffect(() => {
+    setIsLoading(true);
 
+    if (isLoading) {
+      const dispatchPokemons = (
+        currentPage,
+        selectedType,
+        selectedOrder,
+        searchTerm
+      ) => {
+        
+        dispatch(
+          fetchPokemons({
+            currentPage,
+            selectedType,
+            selectedOrder,
+            searchTerm,
+          })
+        )
+          .then((response) => {
+            const data = response.payload.data;
+            const totalPokemons = response.payload.dataLength;
+            setTotalPages(Math.ceil(Math.floor(totalPokemons) / 12));
+            setPokemones(data);
+          })
+          .then(() => {
+            setIsLoading(false);
+          })
+          .catch((error) => {
+            console.error("Error when getting pokemons", error);
+          });
+      };
+
+      if (selectedType === "All") {
+        dispatchPokemons(currentPage, "", selectedOrder, searchTerm);
+      } else {
+        dispatchPokemons(currentPage, selectedType, selectedOrder, searchTerm);
+      }
+    }
+  }, [
+    dispatch,
+    currentPage,
+    isLoading,
+    selectedType,
+    allTypes,
+    selectedOrder,
+    searchTerm,
+  ]);
+
+  
   useEffect(() => {
     !isLoading && setIsLoading(false);
   }, [isLoading]);
 
-  //manejador para cambiar las paginas
   const handlePageChange = (currentPage) => {
     setCurrentPage(currentPage);
-    setSearchPage(currentPage);
-    setPokemones([]); //vacio los pokemons existentes para volver a mostrar el loader
+    setPokemones([]); 
   };
 
-  const totalPokemons = 1281;
-  const totalPages = Math.ceil(totalPokemons / 12);
 
+  useEffect(() => {
+    dispatch(fetchAllTypes(allTypes)).then((response) => {
+      const data = response.payload;
+      setAllTypes(data);
+    });
+  }, [dispatch]);
 
-    return (
-        <div>
-            <h1>Pokemons:</h1>
-            {isLoading ? (
-              <Loader />
-            ) : (
-              pokemones?.map((pokemon) => (
-                <Link key={pokemon.id} to={`/home/pokemons/${pokemon.name}`} className={styles.links}>
-                 <PokemonCard key={pokemon.id} pokemon={pokemon} /> 
-                </Link>
-            )) 
-            )}
+  return (
+    <div>
+      <div>
+        <FiltersBar
+          allTypes={allTypes}
+          setSelectedType={setSelectedType}
+          setSelectedOrder={setSelectedOrder}
+          selectedOrder={selectedOrder}
+        />
+      </div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        pokemones?.map((pokemon) => (
+          <Link
+            key={pokemon?.id}
+            to={`/home/pokemons/${pokemon?.name}`}
+            className={styles.links}
+          >
+            <PokemonCard key={pokemon?.id} pokemon={pokemon} />
+          </Link>
+        ))
+      )}
 
-            <Pagination 
-              currentPage={currentPage}
-              totalPages={totalPages}
-              onPageChange={handlePageChange}
-              isLoading={isLoading}
-              onPageInputChange = {value => setSearchPage(value)}
-            />
-            
-       </div>
-    )
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+        isLoading={isLoading}
+      />
+    </div>
+  );
 };
 
 export default SideBarLeft;
---
+++ client/src/constants.js
@@ -0,0 +1,2 @@
+export const POKEMONS_API_URL = "http://localhost:3001/pokemons";
+export const TYPES_API_URL = "http://localhost:3001/types";
